18b2e5ef5a1853a928e9095a5c9f93a3ac245de2
Manuel Casanova <casanova_manuel@yahoo.com>
2023-10-05 18:04:51 -0300

    elementos pre entrega 1

---
+++ src/components/Loquillo.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Loquillo = ({textito}) => {
+  console.log(textito);
+  return (
+    <div> un loco suelto dice {textito} </div>
+  )
+}
+
+export default Loquillo
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import CartWidget from './CartWidget'
+
+const NavBar = () => {
+  return (
+    <>
+      <div width="20%"><h3>Panda Bureau</h3></div>
+      <div width="20%"><h3>Panda Bureau</h3></div>
+      <div width="20%"><CartWidget/></div>
+      
+    </> 
+  )
+}
+export default NavBar
